4080a9b9413b9b945c7070a2210dbe75b6953692
Andre Herbst <moormaster@gmx.net>
2024-07-21 17:43:26 +0200

    fix: ensure every parsed timestamp contains timezone info

--- events.py
+++ events.py
@@ -65,7 +65,8 @@ def parse_tstamp(metadata, field_name):
     :returns: datetime
     """
     try:
-        return datetime.strptime(metadata[field_name], '%Y-%m-%d %H:%M')
+        # assume local system timezone when parsing datetime
+        return datetime.strptime(metadata[field_name], '%Y-%m-%d %H:%M').astimezone()
     except Exception as e:
         log.error("Unable to parse the '%s' field in the event named '%s': %s" \
             % (field_name, metadata['title'], e))
@@ -171,8 +172,8 @@ def insert_recurring_events(generator):
                 'event-location' : event['location']
             }),
             'event_plugin_data': dict({
-                'dtstart': next_occurrence,
-                'dtend': next_occurrence + event_duration,
+                'dtstart': next_occurrence.astimezone(),
+                'dtend': next_occurrence.astimezone() + event_duration,
             })
         })
         events.append(event)
@@ -202,7 +203,7 @@ def generate_ical_file(generator):
     DEFAULT_LANG = generator.settings['DEFAULT_LANG']
     curr_events = events if not localized_events else localized_events[DEFAULT_LANG]
 
-    filtered_list = filter(lambda x: x.event_plugin_data["dtstart"] >= datetime.now(), curr_events)
+    filtered_list = filter(lambda x: x.event_plugin_data["dtstart"] >= datetime.now().astimezone(), curr_events)
 
     for e in filtered_list:
         icalendar_event = icalendar.Event(
